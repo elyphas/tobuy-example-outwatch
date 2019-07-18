5d508aa0339ddb972c886cb6b617d13d9a6f95d2
elyphas <jctaurys@gmail.com>
2019-07-18 13:20:51 -0500

    update help

--- client/target/scala-2.12/scalajs-bundler/main/scalajs.webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  "entry": {
-    "client-fastopt": ["/home/elyphas/PrjsScalaWS/tobuy/client/target/scala-2.12/scalajs-bundler/main/client-fastopt.js"]
-  },
-  "output": {
-    "path": "/home/elyphas/PrjsScalaWS/tobuy/client/target/scala-2.12/scalajs-bundler/main",
-    "filename": "[name]-bundle.js"
-  }
-}
